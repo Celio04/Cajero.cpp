53c7bcc8175b951efd93f47879ff30781ae99180
Celio Gonzalez <86205716+Celio04@users.noreply.github.com>
2023-10-22 16:24:58 -0500

    Update cajerouno.cpp

--- cajerouno.cpp
+++ cajerouno.cpp
@@ -1,9 +1,11 @@
-//Programa: seguridad 3 capas                     //Autor: Gonzalez Campuzano Celio
+//Programa: seguridad 3 capas                     
+//Autor: Gonzalez Campuzano Celio
 // 5-10-2023 
 
 #include<iostream>
 using namespace std;
-                                                  int main()
+
+int main()
 {
    int c11=11,c12=22,c13=33,c01,c02,c03;
    cout<<"Ingrese las contraseñás c11 c12 c13: ";
