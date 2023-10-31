aeafc06a4b3eb5a9d37b1d7f841d019e260dc790
Celio Gonzalez <86205716+Celio04@users.noreply.github.com>
2023-10-30 20:02:42 -0500

    Update cajero.cpp

--- cajero.cpp
+++ cajero.cpp
@@ -1,1 +1,20 @@
+//Programa: seguridad
+//Autor: Gonzales Campuzano Celio 
+//fecha de creacion: 2-10-2023
 
+
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int c1,c2=5;
+	cout<<"Ingresamos la contraseña: "; cin>>c1;
+	if (c1==c2)
+	{
+		cout<<"Acceso correcto";
+	}else{
+		cout<<"Acceso incorrecto";
+	}
+}
